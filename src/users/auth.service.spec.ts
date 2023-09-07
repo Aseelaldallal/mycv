359dee26b237168fda30bde7de49ff22e963b91d
aseelaldallal <aseelaldallal@gmail.com>
2023-09-06 21:01:42 -0700

    blah

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,31 +1,44 @@
-import { Test } from "@nestjs/testing";
-import { AuthService } from "./auth.service";
-import { UsersService } from "./users.service";
-import { User } from "./users.entity";
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './users.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 let service: AuthService;
+let fakeUsersService: Partial<UsersService>;
 
-describe ('AuthService', () => {
-  beforeEach(async()=> {
+describe('AuthService', () => {
+  beforeEach(async () => {
+    const users: User[] = [];
     // Create a fake copy of usersService
-    const fakeUsersService: Partial<UsersService> = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) => 
-        Promise.resolve({ id: 1, email, password } as User)
-    }
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
+    };
     // Create temporary testing DI container
     const module = await Test.createTestingModule({
       providers: [
-        AuthService, 
+        AuthService,
         {
           provide: UsersService,
-          useValue: fakeUsersService
-        }
-      ]
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
-    service = module.get(AuthService); 
+    service = module.get(AuthService);
   });
-  
+
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
@@ -36,5 +49,31 @@ describe ('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an errors if users signs up with email that is in use', async () => {
+    await service.signup('asdf@asdf.com', 'asdf');
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws if sign in is called with an unused email', async () => {
+    await expect(
+      service.signin('asdffsfsfs@gmail.com', 'assssft'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
+  it('throws if an invalid password is provided', async () => {
+    await service.signup('asdf@asdf.com', 'floo');
+    await expect(service.signin('asdf@asdf.com', 'floooo')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('returns a user if correct password is provided', async () => {
+    await service.signup('asdf@asdf.com', 'mypassword');
+    const user = await service.signin('asdf@asdf.com', 'mypassword');
+    expect(user).toBeDefined();
   });
 });
