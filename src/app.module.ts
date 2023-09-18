78ec3fd991673468e88bd2fa41db4d16688654cf
aseelaldallal <aseelaldallal@gmail.com>
2023-09-18 11:50:53 -0700

    stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,7 +6,9 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
-
+import { ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import cookieSession = require('cookie-session');
 @Module({
   imports: [
     UsersModule,
@@ -19,6 +21,25 @@ import { Report } from './reports/reports.entity';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  // Whenever we create an instance of our app module,
+  // automatically make use of this and apply it into every single
+  // request that is coming into our application - run it through
+  // this instance of a class
+  providers: [AppService, {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(
+      cookieSession({
+        keys: ['seriesofrandomnumbers'], // used to encrypt info inside cookie
+      }),
+    ).forRoutes('*'); // Make use of this middleware in EVERY single request that flows into our app
+  }
+}
